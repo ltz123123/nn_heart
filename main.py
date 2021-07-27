2be5447b64221cbcb82d6cab4de4e042be5fcb81
ltz123123 <justere908@gmail.com>
2021-07-27 17:02:13 +0800

    add the last 2 features back to training dataset

--- main.py
+++ main.py
@@ -7,45 +7,48 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+import tensorflow.keras.backend as K
+
+
+K.clear_session()
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
 
 def preprocessing():
     df = pd.read_csv("heart.csv")
 
-    targets = df["target"]
-    features = df.drop(columns=["ca", "thal", "target"])
-
-    cat_cols = ["sex", "cp", "fbs", "restecg", "exang", "slope"]
+    cat_cols = ["sex", "cp", "fbs", "restecg", "exang", "slope", "ca", "thal"]
     num_cols = ["age", "trestbps", "chol", "thalach", "oldpeak"]
 
-    # create dummies for categorical features
+    df = df[(df["ca"] < 4) & (df["thal"] > 0)]
+    targets = df["target"]
+    features = df.drop(columns=["target"])
     features = pd.get_dummies(features, columns=cat_cols)
 
-    # standardize numerical columns
-    features_train, features_test, targets_train, targets_test = train_test_split(features, targets, test_size=0.2)
+    # standardize numerical features
+    features_train, features_test, targets_train, targets_test = train_test_split(features, targets, test_size=0.2, random_state=1)
     scaler = StandardScaler().fit(features_train[num_cols])
-    features_train[num_cols] = scaler.transform(features_train[num_cols])
-    features_test[num_cols] = scaler.transform(features_test[num_cols])
+    features_train.loc[:, features_train.columns.isin(num_cols)] = scaler.transform(features_train[num_cols])
+    features_test.loc[:, features_test.columns.isin(num_cols)] = scaler.transform(features_test[num_cols])
 
     return features_train, features_test, targets_train, targets_test
 
 
 def build_model(input_dim):
     model = Sequential([
-        Dense(256, activation="relu", input_dim=input_dim),
+        Dense(128, activation="relu", kernel_initializer="he_uniform", input_dim=input_dim),
         BatchNormalization(),
         Dropout(0.1),
-        Dense(128, activation="relu"),
+        Dense(256, activation="relu", kernel_initializer="he_uniform"),
         BatchNormalization(),
         Dropout(0.1),
-        Dense(128, activation="relu"),
-        BatchNormalization(),
-        Dropout(0.1),
-        Dense(1, activation="sigmoid")
+        Dense(1, activation="sigmoid", kernel_initializer="he_uniform")
     ])
 
     model.compile(
-        optimizer=Adam(lr=0.001),
+        optimizer=Adam(learning_rate=5e-5),
         loss="binary_crossentropy",
         metrics=["accuracy"]
     )
@@ -68,20 +71,15 @@ def train():
 
     model.fit(
         x_train, y_train,
-        validation_split=0.2,
-        epochs=50,
-        batch_size=64,
+        epochs=100,
+        batch_size=32,
         verbose=2,
         shuffle=True,
-        callbacks=[
-            tensorboard,
-            # checkpoint
-        ]
-    )
-
-    model.evaluate(
-        x_test, y_test,
-        verbose=2
+        validation_data=(x_test, y_test)
+        # callbacks=[
+        #     tensorboard,
+        #     checkpoint
+        # ]
     )
 
 
